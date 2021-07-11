8d63ca32c5dba415414e9138163e73dfe9003d19
moidryg <nazar.kryzhanivskyii@gmail.com>
2021-07-11 15:58:15 +0300

    first commit

---
+++ src/components/catalog/v-catalog.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class='v-catalog'>
+    <router-link :to="{name: 'cart', params: {cart_data: CART}}">
+      <div class="v-catalog__link_to_cart">Cart: {{CART.length}}</div>
+    </router-link>
+    <h1>Catalog</h1>
+        <div class="filters">
+      <v-select
+          :selected="selected"
+          :options="categories"
+          @select="sortByCategories"
+      />
+      <div class="range-slider">
+        <input
+            type="range"
+            min="0"
+            max="1000"
+            step="10"
+            v-model.number="minPrice"
+            @change="setRangeSlider"
+        >
+        <input
+            type="range"
+            min="0"
+            max="1000"
+            step="10"
+            v-model.number="maxPrice"
+            @change="setRangeSlider"
+        >
+      </div>
+      <div class="range-values">
+        <p>Min: {{minPrice}}</p>
+        <p>Max: {{maxPrice}}</p>
+      </div>
+    </div>
+   
+ 
+    <div class="v-catalog__list">
+      <v-catalog-item
+          v-for="product in filteredProducts"
+          :key="product.article"
+          :product_data="product"
+          @addToCart="addToCart"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+  import vCatalogItem from './v-catalog-item'
+  import {mapActions, mapGetters} from 'vuex'
+  import VSelect from '../v-select'
+  export default {
+    name: "v-catalog",
+    components: {
+      vCatalogItem,
+        VSelect
+    },
+    props: {},
+    data() {
+      return {
+        categories: [
+          {name: 'Все', value: 'ALL'},
+          {name: 'Мужские', value: 'м'},
+          {name: 'Женские', value: 'ж'},
+        ],
+        selected: 'Все',
+        sortedProducts: [],
+        minPrice:0,
+        maxPrice:1000
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'PRODUCTS',
+        'CART'
+      ]),
+    
+      filteredProducts(){
+        if(this.sortedProducts.length){
+           return this.sortedProducts 
+         } else {
+           return this.PRODUCTS
+         }
+      },
+    },
+    methods: {
+      ...mapActions([
+        'GET_PRODUCTS_FROM_API',
+        'ADD_TO_CART'
+      ]),
+         setRangeSlider() {
+        if (this.minPrice > this.maxPrice) {
+          let tmp = this.maxPrice;
+          this.maxPrice = this.minPrice;
+          this.minPrice = tmp;
+        }
+        this.sortByCategories()
+      },
+      sortByCategories(category) {
+        let vm = this;
+        this.sortedProducts = [...this.PRODUCTS]
+        this.sortedProducts = this.sortedProducts.filter(function (item) {
+          return item.price >= vm.minPrice && item.price <= vm.maxPrice
+        })
+        if (category) {
+          this.sortedProducts = this.sortedProducts.filter(function (e) {
+            vm.selected === category.name;
+            return e.category === category.name
+          })
+        }
+      },
+      addToCart(data) {
+        this.ADD_TO_CART(data)
+      }
+    },
+    mounted() {
+      this.GET_PRODUCTS_FROM_API()
+      .then((response) => {
+        if (response.data) {
+          console.log('Data arrived!')
+          this.sortByCategories()
+        }
+      })
+    }
+  }
+</script>
+
+<style lang="scss">
+  .v-catalog {
+    &__list {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      align-items: center;
+    }
+    &__link_to_cart {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      padding: $padding*2;
+      border: solid 1px #aeaeae;
+    }
+  }
+
+    .filters {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .range-slider {
+    width: 200px;
+    margin: auto 16px;
+    text-align: center;
+    position: relative;
+  }
+  .range-slider svg, .range-slider input[type=range] {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+  }
+  input[type=range]::-webkit-slider-thumb {
+    z-index: 2;
+    position: relative;
+    top: 2px;
+    margin-top: -7px;
+  }
+</style>
---
+++ src/vuex/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import actions from './actions/actions'
+import getters from './getters/getters'
+import mutations from './mutations/mutations'
+
+Vue.use(Vuex);
+
+let store = new Vuex.Store({
+  state: {
+    products: [],
+    cart: []
+  },
+  mutations,
+  actions,
+  getters
+});
+
+export default store;
